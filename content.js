7867e35f991b990f19b4976385cee5a2e69557aa
Joshua Harris <meshuggie@gmail.com>
2017-02-09 10:43:11 -0800

    update to have expand icon

--- content.js
+++ content.js
@@ -23,7 +23,7 @@
       k2Sites.forEach(function(site) {
           if ( $('.sod_option[title*="' + site + '"]').length > 1 ) {
               tamperMenuHtml += '<span class="sod_container" style="position: relative; display: block; white-space: nowrap; width: 100%;">';
-              tamperMenuHtml += '<span class="sod_option" title="' + site + '" style="padding: 10px; margin: 5px 0;">' + site + '</span>';
+              tamperMenuHtml += '<span class="sod_option" title="' + site + '" style="padding: 10px; margin: 5px 0;">' + site + '<span style="position: absolute;transform: translate(0, -50%);top: 46%;right: 7px;font-weight: bold;font-size: 24px;">&rsaquo;</span></span>';
               tamperMenuHtml += '<span class="sod_flyout" style="position: absolute; top: 0; left: calc(100% + 1px); box-shadow: 0 3px 5px #999; width: 100%; background: #fff; display: none;">';
               $('.sod_option[title*="' + site + '"]').each(function() {
                   var elem = $('<span>');
